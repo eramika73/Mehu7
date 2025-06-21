57e3fc881024cb3d7db6b8abe9a6de04323029d7
eramika73 <themh699894@gmail.com>
2025-06-21 09:52:12 +0600

    Update Ullash.js

--- Ullash.js
+++ Ullash.js
@@ -1,77 +1,92 @@
-const { spawn } = require("child_process");
-const axios = require("axios");
-const logger = require("./utils/log");
-
-///////////////////////////////////////////////////////////
-//========= Create website for dashboard/uptime =========//
-///////////////////////////////////////////////////////////
-
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const port = process.env.PORT || 8080;
-
-// Serve the index.html file
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-// Start the server and add error handling
-app.listen(port, () => {
-    logger(`Server is running on port ${port}...`, "[ Starting ]");
-}).on('error', (err) => {
-    if (err.code === 'EACCES') {
-        logger(`Permission denied. Cannot bind to port ${port}.`, "[ Error ]");
-    } else {
-        logger(`Server error: ${err.message}`, "[ Error ]");
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261553588714169%22%3A%22UVTftyAC%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.277Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "sb",
+        "value": "Tm5VaG7HtsVgjCjZ9yEIWIkC",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "wd",
+        "value": "1366x641",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "datr",
+        "value": "sH1VaGnAAMHk54pIN_0Ax7db",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61553588714169",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "xs",
+        "value": "21%3AeAbaJqrQ0JKSkg%3A2%3A1750477804%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "fr",
+        "value": "1SP8oORCTC3fHozfu.AWc6tDRy8mHvrfnok6bMnX4nYlM7SNl2EyggaSLmqOw8Ri8XMdI.BoVZLA..AAA.0.0.BoVivv.AWf-mQPpAzcTd3hPfuNrPAGCykI",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1750477710080%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-06-21T03:48:33.280Z",
+        "lastAccessed": "2025-06-21T03:48:33.280Z"
     }
-});
-
-/////////////////////////////////////////////////////////
-//========= Create start bot and make it loop =========//
-/////////////////////////////////////////////////////////
-
-// Initialize global restart counter
-global.countRestart = global.countRestart || 0;
-
-function startBot(message) {
-    if (message) logger(message, "[ Starting ]");
-
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Cyber.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
-
-    child.on("close", (codeExit) => {
-        if (codeExit !== 0 && global.countRestart < 5) {
-            global.countRestart += 1;
-            logger(`Bot exited with code ${codeExit}. Restarting... (${global.countRestart}/5)`, "[ Restarting ]");
-            startBot();
-        } else {
-            logger(`Bot stopped after ${global.countRestart} restarts.`, "[ Stopped ]");
-        }
-    });
-
-    child.on("error", (error) => {
-        logger(`An error occurred: ${JSON.stringify(error)}`, "[ Error ]");
-    });
-};
-
-////////////////////////////////////////////////
-//========= Check update from Github =========//
-////////////////////////////////////////////////
-
-axios.get("https://raw.githubusercontent.com/cyber-ullash/cyber-bot/main/data.json")
-    .then((res) => {
-        logger(res.data.name, "[ NAME ]");
-        logger(`Version: ${res.data.version}`, "[ VERSION ]");
-        logger(res.data.description, "[ DESCRIPTION ]");
-    })
-    .catch((err) => {
-        logger(`Failed to fetch update info: ${err.message}`, "[ Update Error ]");
-    });
-
-// Start the bot
-startBot();
+]
